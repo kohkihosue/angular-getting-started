b47343418baa3acf65f88d99cda0d321efa8d470
kohkihosue <k.kohki@gmail.com>
2021-12-01 22:33:36 +0900

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,16 +5,21 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
+
+  students = [
+    { name: '一郎', gender: '男', age: 40 },
+    { name: '二郎', gender: '男', age: 38 },
+    { name: '三郎', gender: '男', age: 34 },
+  ];
 
   share() {
     window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
